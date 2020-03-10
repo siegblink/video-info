f4e7b3cb4f903ddee8547035975833ef7acd7d3a
Sieg <siegblink@gmail.com>
2020-03-10 15:23:30 +0800

    Refactor index.js file.

--- index.js
+++ index.js
@@ -2,17 +2,39 @@ const electron = require('electron')
 const ffmpeg = require('fluent-ffmpeg')
 
 const { app, BrowserWindow, ipcMain } = electron
-let mainWindow
+// let mainWindow
 
-app.on('ready', () => {
-  mainWindow = new BrowserWindow({
-    webPreferences: { nodeIntegration: true },
+// app.on('ready', () => {
+//   mainWindow = new BrowserWindow({
+//     webPreferences: { nodeIntegration: true },
+//   })
+//   mainWindow.loadURL(`file://${__dirname}/index.html`)
+// })
+
+// ipcMain.on('video:submit', (event, path) => {
+//   ffmpeg.ffprobe(path, (err, metadata) => {
+//     mainWindow.webContents.send('video:metadata', metadata.format.duration)
+//   })
+// })
+
+let window
+
+function createWindow() {
+  // Create browser window
+  window = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
   })
-  mainWindow.loadURL(`file://${__dirname}/index.html`)
-})
+  window.loadFile('index.html')
+}
+
+app.whenReady().then(createWindow)
 
 ipcMain.on('video:submit', (event, path) => {
   ffmpeg.ffprobe(path, (err, metadata) => {
-    mainWindow.webContents.send('video:metadata', metadata.format.duration)
+    window.webContents.send('video:metadata', metadata.format.duration)
   })
 })
